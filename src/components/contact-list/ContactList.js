14586d63700a722f4d3a964e67ac5a52ce05474b
Stas-0k <brickbybrick15@gmail.com>
2023-02-08 12:48:31 +0200

    commit
    commit

---
+++ src/components/contact-list/ContactList.js
@@ -0,0 +1,32 @@
+import propTypes from 'prop-types';
+import css from './contact-list.module.css';
+
+const ContactList = ({ contacts, onDelete }) => {
+  if (contacts.length > 0) {
+    return (
+      <ul className={css.list_contact}>
+        {contacts.map(contact => (
+          <li className={css.listItem_contact} key={contact.id}>
+            {contact.name}: {contact.number}
+            <button
+              className={css.bttn_contactList}
+              type="button"
+              onClick={() => onDelete(contact.id)}
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    );
+  } else {
+    return <p>No data to display :(</p>;
+  }
+};
+
+ContactList.propTypes = {
+  contacts: propTypes.array.isRequired,
+  onDelete: propTypes.func.isRequired,
+};
+
+export default ContactList;
